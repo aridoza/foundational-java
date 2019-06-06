9ee8f5e8a295dfc335313118a29fb43bedbe9ffc
Victor Grazi <vgrazi@gmail.com>
2019-06-05 21:10:36 -0400

    removed more duplicate files

---
+++ Concurrency/src/com/generalassembly/concurrency/ConcurrencyLesson.java
@@ -0,0 +1,12 @@
+package com.generalassembly.concurrency;
+
+public class ConcurrencyLesson {
+    public static void main(String[] args) {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+            }
+        }
+    }
+}
